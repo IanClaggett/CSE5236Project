74c42fe739d4e2e56a24311551e464cbef214d8d
Lolozo <samsimonzhang@gmail.com>
2025-02-21 16:12:56 -0500

    added fragment skeletons

--- app/src/main/java/com/example/cse5236/MainActivity.kt
+++ app/src/main/java/com/example/cse5236/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.example.cse5236
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         println("OnCreate")
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val btnFragment1: Button = findViewById(R.id.fragment_blank1)
+        val btnFragment2: Button = findViewById(R.id.fragment_blank2)
+        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        btnFragment1.setOnClickListener {
+            // Replace with Fragment1
+            replaceFragment(BlankFragment1())
+        }
+
+        // Button 2 click listener
+        btnFragment2.setOnClickListener {
+            // Replace with Fragment2
+            replaceFragment(BlankFragment2())
         }
     }
 
@@ -42,5 +57,12 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         println("onDestroy()")
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
