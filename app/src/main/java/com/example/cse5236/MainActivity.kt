899da757c6daf692fac837e468bad2e40b9a7eaf
Ian Claggett <claggett.32@buckeyemail.osu.edu>
2025-03-03 10:59:11 -0500

    Updated UI for sign in and sign up screens

--- app/src/main/java/com/example/cse5236/MainActivity.kt
+++ app/src/main/java/com/example/cse5236/MainActivity.kt
@@ -1,68 +1,48 @@
 package com.example.cse5236
 
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        println("OnCreate")
-        setContentView(R.layout.activity_main)
-        val btnFragment1: Button = findViewById(R.id.fragment_blank1)
-        val btnFragment2: Button = findViewById(R.id.fragment_blank2)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        btnFragment1.setOnClickListener {
-            // Replace with Fragment1
-            replaceFragment(BlankFragment1())
+        val signUp = SignUp()
+        val signIn = SignIn()
+
+        val btnSignIn = findViewById<View>(R.id.btnSignIn)
+        val btnSignUp = findViewById<View>(R.id.btnSignUp)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.FrameFragmentView, signUp)
+            commit()
         }
 
-        // Button 2 click listener
-        btnFragment2.setOnClickListener {
-            // Replace with Fragment2
-            replaceFragment(BlankFragment2())
+        btnSignIn.setOnClickListener(){
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.FrameFragmentView, signIn)
+                commit()
+            }
         }
-    }
 
-    override fun onStart() {
-        super.onStart()
-        println("onStart()")
-    }
+        btnSignUp.setOnClickListener(){
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.FrameFragmentView, signUp)
+                commit()
+            }
+        }
 
-    override fun onResume() {
-        super.onResume()
-        println("onResume()")
-    }
 
-    override fun onPause() {
-        super.onPause()
-        println("onPause()")
-    }
 
-    override fun onStop(){
-        super.onStop()
-        println("onStop()")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        println("onDestroy()")
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
     }
 }
--- app/src/main/java/com/example/cse5236/SignIn.kt
+++ app/src/main/java/com/example/cse5236/SignIn.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BlankFragment2.newInstance] factory method to
+ * Use the [SignUp.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BlankFragment2 : Fragment() {
+class SignIn : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,14 +34,10 @@ class BlankFragment2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        println("onCreateView() for fragment2")
-        return inflater.inflate(R.layout.fragment_blank2, container, false)
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        println("onDestroyView() for fragment2")
-    }
+
 
     companion object {
         /**
@@ -50,12 +46,12 @@ class BlankFragment2 : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment2.
+         * @return A new instance of fragment SignUp.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BlankFragment2().apply {
+            SignUp().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
